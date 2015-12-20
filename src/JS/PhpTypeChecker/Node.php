73afca73f98016c175d55d08e1ba2736479c4d45
Jesse Schalken <me@jesseschalken.com>
2015-12-20 23:50:59 +1100

    Try to use PHP 7 scalar/return type hints

--- src/JS/PhpTypeChecker/Node.php
+++ src/JS/PhpTypeChecker/Node.php
@@ -31,13 +31,7 @@ class Parser {
         return $stmts;
     }
 
-    /**
-     * @param Node       $node
-     * @param Namespace_ $ns
-     * @return Stmt
-     * @throws \Exception
-     */
-    private static function parseStmt(Node $node, Namespace_ $ns) {
+    private static function parseStmt(Node $node, Namespace_ $ns):Stmt {
         if ($node instanceof Node\Expr) {
             return self::parseExpr($node, $ns);
         } else if ($node instanceof Node\Stmt\If_) {
@@ -68,13 +62,7 @@ class Parser {
         }
     }
 
-    /**
-     * @param Node\Expr  $node
-     * @param Namespace_ $ns
-     * @return Expr
-     * @throws \Exception
-     */
-    private static function parseExpr(Node\Expr $node, Namespace_ $ns) {
+    private static function parseExpr(Node\Expr $node, Namespace_ $ns):Expr {
         if ($node instanceof Node\Expr\ConstFetch) {
             return new ConstFetch($ns->resolveConst($node->name));
         } else if ($node instanceof Node\Expr\Assign) {
@@ -90,13 +78,7 @@ class Parser {
         }
     }
 
-    /**
-     * @param Node\Expr  $expr
-     * @param Namespace_ $ns
-     * @return LValue
-     * @throws \Exception
-     */
-    private static function parseLValue(Node\Expr $expr, Namespace_ $ns) {
+    private static function parseLValue(Node\Expr $expr, Namespace_ $ns):LValue {
         if ($expr instanceof Node\Expr\Variable) {
             if (is_string($expr->name)) {
                 return new Variable($expr->name);
@@ -119,34 +101,19 @@ class Namespace_ {
     /** @var string[] */
     private $useConst = [];
 
-    /**
-     * @param string $prefix
-     */
-    function __construct($prefix) {
+    function __construct(string $prefix) {
         $this->prefix = $prefix;
     }
 
-    /**
-     * @param Node\Name $name
-     * @return string
-     */
-    function resolveClass(Node\Name $name) {
+    function resolveClass(Node\Name $name):string {
         return $this->resolve($name, $this->useClass);
     }
 
-    /**
-     * @param Node\Name $name
-     * @return string
-     */
-    function resolveConst(Node\Name $name) {
+    function resolveConst(Node\Name $name):string {
         return $this->resolve($name, $this->useConst);
     }
 
-    /**
-     * @param Node\Name $name
-     * @return string
-     */
-    function resolveFunction(Node\Name $name) {
+    function resolveFunction(Node\Name $name):string {
         return $this->resolve($name, $this->useFunction);
     }
 
@@ -188,7 +155,7 @@ class If_ extends Stmt {
      * @param Stmt[] $true
      * @param stmt[] $false
      */
-    public function __construct($cond, array $true, array $false) {
+    public function __construct(Expr $cond, array $true, array $false) {
         $this->cond  = $cond;
         $this->true  = $true;
         $this->false = $false;
@@ -199,15 +166,10 @@ abstract class Expr extends Stmt {
 }
 
 class ConstFetch extends Expr {
-    /**
-     * @var string
-     */
+    /** @var string */
     private $name;
 
-    /**
-     * @param string $name
-     */
-    public function __construct($name) {
+    public function __construct(string $name) {
         $this->name = $name;
     }
 }
@@ -216,9 +178,6 @@ class Return_ extends Stmt {
     /** @var Expr|null */
     private $expr;
 
-    /**
-     * @param Expr|null $expr
-     */
     public function __construct(Expr $expr = null) {
         $this->expr = $expr;
     }
@@ -232,12 +191,7 @@ class Assign extends Expr {
     /** @var Expr */
     private $right;
 
-    /**
-     * @param LValue $left
-     * @param Expr   $right
-     * @param bool   $byRef
-     */
-    public function __construct(LValue $left, Expr $right, $byRef = false) {
+    public function __construct(LValue $left, Expr $right, bool $byRef = false) {
         $this->left  = $left;
         $this->right = $right;
         $this->byRef = $byRef;
@@ -252,10 +206,7 @@ class Variable extends LValue {
     /** @var string */
     private $name;
 
-    /**
-     * @param string $name
-     */
-    public function __construct($name) {
+    public function __construct(string $name) {
         $this->name = $name;
     }
 }
@@ -264,9 +215,6 @@ class DynamicVariable extends LValue {
     /** @var Expr */
     private $expr;
 
-    /**
-     * @param Expr $expr
-     */
     public function __construct(Expr $expr) {
         $this->expr = $expr;
     }
@@ -276,10 +224,7 @@ class Int_ extends Expr {
     /** @var int */
     private $value;
 
-    /**
-     * @param int $value
-     */
-    function __construct($value) {
+    function __construct(int $value) {
         $this->value = $value;
     }
 }
@@ -288,10 +233,7 @@ class Float_ extends Expr {
     /** @var float */
     private $value;
 
-    /**
-     * @param float $value
-     */
-    public function __construct($value) {
+    public function __construct(float $value) {
         $this->value = $value;
     }
 }
@@ -300,10 +242,7 @@ class String_ extends Expr {
     /** @var string */
     private $value;
 
-    /**
-     * @param string $value
-     */
-    public function __construct($value) {
+    public function __construct(string $value) {
         $this->value = $value;
     }
 }
